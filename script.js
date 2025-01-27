73d120ffb43b0070de2b178aa486c2b5ef9dce2c
Bhanu Partap <65064668+bhanupartapsaini@users.noreply.github.com>
2025-01-27 13:22:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,211 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34; // width/height ratio = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+
+// let birdImg;
+let birdImgs = [];
+let birdImgsIndex = 0;
+
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+//pipes
+let pipeArray = [];
+let pipeWidth = 64; //width/height ratio = 384/3072 =1/8
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//physics
+let velocityX = -2; //pipes moving left speed
+let velocityY = 0; // bird jump speed
+let gravity = 0.4;
+
+let gameOver = false;
+let score =0;
+
+//sounds
+let wingSound = new Audio("./assets/sfx_wing.wav");
+let hitSound =  new Audio ("./assets/sfx_hit.wav");
+let bgm = new Audio("./assets/bgm_mario.mp3");
+bgm.loop = true;
+
+let dieSound = new Audio ("./assets/sfx_die.wav");
+let point = new Audio ("./assets/sfx_point.wav");
+let swooshing = new Audio ("./assets/sfx_swooshing.wav");
+
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
+
+  //draw flappy bird
+  // context.fillStyle = "green";
+  // context.fillRect (bird.x, bird.y, bird.width, bird.height);
+
+  //load images
+  // birdImg = new Image();
+  // birdImg.src = "./assets/flappybird.png";
+  // birdImg.onload = function () {
+  //   context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  // }
+for (let i = 0; i< 4; i++){
+  let birdImg = new Image();
+  birdImg.src = `./assets/flappybird${i}.png`;
+  birdImgs.push(birdImg); 
+}
+  topPipeImg = new Image();
+  topPipeImg.src = "./assets/toppipe.png";
+
+  bottomPipeImg = new Image();
+  bottomPipeImg.src = "./assets/bottompipe.png";
+
+
+  requestAnimationFrame(update);
+  setInterval(placePipes, 1500); //every 1.5 seconds
+  setInterval(animateBird, 100); 
+  document.addEventListener("keydown", moveBird);
+}
+
+function update() {
+  requestAnimationFrame(update);
+  if(gameOver){
+    return;
+  }
+  context.clearRect(0, 0, board.width, board.height);
+
+  //bird
+  velocityY += gravity;
+  if (velocityY > 0 && !swooshing.playing) { // Check if bird is falling
+    swooshing.play();
+  }
+  bird.y = Math.max(bird.y + velocityY, 0);
+  // context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  context.drawImage(birdImgs[birdImgsIndex], bird.x, bird.y, bird.width, bird.height);
+
+
+  if(bird.y > board.height){
+    dieSound.play();
+    gameOver = true;
+  }
+
+  //pipes
+  for (let i = 0; i < pipeArray.length; i++) {
+    let pipe = pipeArray[i];
+    pipe.x += velocityX;
+    context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    if(!pipe.passed && bird.x > pipe.x + pipe.width){
+      score += 0.5;
+      pipe.passed = true;
+      point.play();
+    }
+
+    if (detectCollision(bird, pipe)) {
+      hitSound.play();
+      gameOver = true;
+    }
+  }
+
+  //clear pipes
+  while(pipeArray.length > 0 && pipeArray[0].x < -pipeWidth){
+    pipeArray.shift();
+  }
+
+  //score
+  context.fillStyle = "white";
+  context.font = " 45px sans-serif";
+  context.fillText(score, 5, 45);
+
+
+  if (gameOver) {
+    context.fillText("GAME OVER", 5, 90);
+    bgm.pause();
+}
+}
+
+function animateBird(){
+  birdImgsIndex++;
+  birdImgsIndex %= birdImgs.length;
+}
+
+function placePipes() {
+
+  if(gameOver){
+    return;
+  }
+  //(0-1)* pipeHeight/2.
+  //0 -> -128(pipeHeight/4)
+  //1 ->  -128 -256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
+
+  let randomPipeY = pipeY - pipeHeight/4 - Math.random() * (pipeHeight / 2);
+  let openingSpace = board.height / 4;
+    
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomPipeY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+  pipeArray.push(topPipe);
+
+  let bottomPipe = {
+    img: bottomPipeImg,
+    x: pipeX,
+    y: randomPipeY + pipeHeight + openingSpace,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+  pipeArray.push(bottomPipe);
+}
+function moveBird(e) {
+  if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+    if(bgm.paused){
+    bgm.play();
+    }
+
+    wingSound.play();
+    
+
+    //jump
+    velocityY = -6;
+
+    //reset game
+    if(gameOver){
+      bird.y = birdY;
+      pipeArray=[];
+      score = 0;
+  gameOver = false;
+  
+    }
+  }
+}
+
+function detectCollision(a, b) {
+  return   a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y;
+    
+}
